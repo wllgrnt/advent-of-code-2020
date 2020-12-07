424ee083de3b5ae9f5b13af44b2930104ebff70f
William Grant <wpg23@cam.ac.uk>
2020-12-07 11:22:09 -0800

    Part 2

--- day_7/day7.py
+++ day_7/day7.py
@@ -25,16 +25,23 @@ So, in this example, the number of bag colors that can eventually contain at lea
 """
 import re
 import networkx as nx
-if __name__ == "__main__":
 
-    with open("input.txt") as flines:
-        bag_rules = [line.strip() for line in flines]
+def get_weight_from_edge_data(G, edge):
+    return int(G.get_edge_data(edge[0], edge[1])["weight"])
 
+def get_edge_weights(G, node):
+    weight = 0
+    if G.edges(node):
+        for edge in G.edges(node):
+            edge_weight = get_weight_from_edge_data(G, edge)
+            weight += edge_weight*get_edge_weights(G, edge[1])
+    return weight+1  # account for the bag itself with the +1
+
+def solve(input_data):
     # We have to generate a directed bag graph, then find the number of paths that lead to "shiny gold".
-
     G = nx.DiGraph()
     # Start by parsing the edges - format is {colour_id} bags contain {number} {colour_id} bag(s) (, {number colour_id} bag(s))... \.
-    for rule in bag_rules:
+    for rule in input_data:
         source, target = rule.split(" bags contain ")
         # print(rule)
         matches =re.findall(r"[0-9]+.*?(?= bag)", target)
@@ -45,14 +52,40 @@ if __name__ == "__main__":
                 match_name = " ".join(match.split(" ")[1: ])
                 G.add_edge(source, match_name, weight=edge_weight)
 
-    matches = 0
-    bag_colours = []
-    for node in G.nodes():
-        if node != "shiny gold" and nx.has_path(G, node, "shiny gold"):
-            matches += 1
-            bag_colours.append(node)
+    # Part one - how many nodes have a path to "shiny gold"?
+    print(len(nx.ancestors(G, "shiny gold")))
 
-    print(bag_colours)
-    print(matches)
-    #  
-     
+    # Part two - how many individual bags are required inside your shiny gold bag?
+    print(get_edge_weights(G, "shiny gold") - 1)
+
+
+if __name__ == "__main__":
+
+    print("Test data:")
+    test_data = """shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags."""
+    test_bag_rules = [line.strip() for line in test_data.split("\n")]
+    solve(test_bag_rules)
+
+    test_data_2 = """light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags."""
+    test_bag_rules_2 = [line.strip() for line in test_data_2.split("\n")]
+    solve(test_bag_rules_2)
+    
+
+    with open("input.txt") as flines:
+        bag_rules = [line.strip() for line in flines]
+    print("Problem:")
+    solve(bag_rules)
